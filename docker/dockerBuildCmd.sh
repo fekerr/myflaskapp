0bae5d7aab6e5ffc7ae14954d74ec0d6fc7b3abe
Fred <fekerr@gmail.com>
2018-11-11 17:33:42 -0800

    updates

--- docker/dockerBuildCmd.sh
+++ docker/dockerBuildCmd.sh
@@ -2,5 +2,5 @@
 # fekerr 20181108 Thu
 # create traversy flask app docker
 
-docker build -t fekerr/travflask:1 ..
+docker build -t fekerr/travflask:1.0 ..
 
--- docker/hostrun.sh
+++ docker/hostrun.sh
@@ -2,7 +2,7 @@
 # fekerr 20181108 Thu
 # This will be the command run on the host to start the travflask docker container
 # run with ". hostrun.sh"
-TRAVFLASKVER=1
+TRAVFLASKVER=1.0
 
 docker container run -t --detach --publish 82:80 --name travflask --mount type=bind,source=$(pwd)../bind,target=/app/bind fekerr/travflask:${TRAVFLASKVER} bash
 
